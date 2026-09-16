1506c76b3fffd6e57f83b9721ea77925db55b356
AzounV <andres.azabache@outlook.com>
2026-09-16 11:14:26 +1000

    Sheep now eat both grass and flowers, though they prioritise flowers

--- src/Sheep.java
+++ src/Sheep.java
@@ -17,12 +17,13 @@ public class Sheep extends Animal{
     public Entity LookForFood()
     {
         //tempo just to allow for logic development. Replace with proper entity database at some point
-        List<Entity> allFlowers = Board.entities.get(Board.Ent.flower.get());
+        // Do flowers first, then grass
+        List<Entity> allPlants = Board.entities.get(Board.Ent.flower.get());
     
 
         Entity closest = null;
         double closestDist = 99999;
-        for (Entity flower : allFlowers) {
+        for (Entity flower : allPlants) {
 
             double dist = this.pos.dist(flower.pos) ;
             if(dist < this.perception && dist < closestDist)
@@ -34,9 +35,23 @@ public class Sheep extends Animal{
         if(closest != null)
         {
             //System.out.println("Closest flower @ " + closest.pos);
+            return closest;
         }else{
             //System.out.println("No flowers in range");
+
         }
+        allPlants = Board.entities.get(Board.Ent.grass.get());
+        closestDist = 99999;
+        for (Entity grass : allPlants) {
+
+            double dist = this.pos.dist(grass.pos) ;
+            if(dist < this.perception && dist < closestDist)
+            {
+                closest = grass;
+                closestDist = dist;
+            }
+        }
+
         return closest;
 
 
